aee8ec1605fd571c34bfe8200063434bc16c8f5d
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2022-11-03 23:13:42 +0100

    Comment

--- mount_hass_crypt_backup.sh
+++ mount_hass_crypt_backup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# Name you choose for the container
+
+# Disk partition to mount
 HDD=/dev/sda1
+# Folder in the disk partition to store backups
 LOCAL_FOLDER=/mnt/data/backup-benoit
+# Folder in the container
 CT_FOLDER=/mnt/backup-benoit
+# Name for the container
 CT_NAME=cryptsetup_container
 
 function quit()
@@ -25,9 +29,12 @@ if [ ! "${CID}" ]; then
   docker commit $CT_NAME $CT_NAME
   docker start $CT_NAME
   #docker run -d -p 22:1979 -v $LOCAL_FOLDER:$CT_FOLDER --name $CT_NAME --privileged --cap-add=ALL $CT_NAME
+  echo "Updating repositories..."
   docker exec $CT_NAME apt update
   #docker exec $CT_NAME apt upgrade
+  echo "Installing dependancies..."
   docker exec $CT_NAME apt install -y cryptsetup openssh-server rsync
+  echo "Allowing root login into the container..."
   docker exec $CT_NAME sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 fi
 unset CID
@@ -39,9 +46,13 @@ if [ ! "${CID}" ]; then
 fi
 unset CID
 
+echo "Starting SSH..."
 docker exec $CT_NAME service ssh start
+echo "Showing IP address..."
 docker inspect $CT_NAME | grep 'IPAddress'
+echo "Opening disk..."
 docker exec -it $CT_NAME cryptsetup luksOpen $HDD hdd_backup
+echo "Mounting disk..."
 docker exec $CT_NAME mount -t ext4 /dev/mapper/hdd_backup $CT_FOLDER
 echo "When done, run :"
 echo " docker exec $CT_NAME umount /dev/mapper/hdd_backup"
