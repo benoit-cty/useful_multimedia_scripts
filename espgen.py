c186be9e6664dc2d4395f9a786a00d560ee4f4a0
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2022-11-03 15:46:28 +0100

    Add problems

--- espgen.py
+++ espgen.py
@@ -1,4 +1,9 @@
-for i, gpio in enumerate([13,12,14,27,26,25,33,32]):
+# GPIO 35 et 34 ne supporte pas Output
+# GPIO 12 empêche le démarrage
+# GPIO2, GPIO0, GPIO4 is a Strapping PIN and should be avoided.
+# 16 pose problème aussi.
+for i, gpio in enumerate([13,14,27,23,25,33,32, 17]):
     print(f'''  - platform: gpio
     pin: {gpio}
-    name: "gpio{gpio}-{i+1}"''')
+    name: "relais_{i+1}-gpio{gpio}"
+    inverted: true''')
